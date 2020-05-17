92859874e348f76e107c9db028ecbfb6c7282cb1
Sergey Chekanskiy <chekanskiy@gmail.com>
2020-05-17 14:47:08 +0200

    prep for deployment

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ geojson = json.load(open('data/data_geo_de.json', 'r'))
 app = dash.Dash(__name__,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],)
 # external_stylesheets=external_stylesheets)
+server = app.server
 
 # ========================================= DEFINE PROPERTIES ================================================
 STATES = {'BW': 'Baden-Wuerttemberg',
