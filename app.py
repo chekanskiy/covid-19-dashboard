1c1bacff17dcfc68bd4b45e489c533b005af5551
Sergey Chekanskiy <chekanskiy@gmail.com>
2020-05-29 18:19:44 +0200

    ga

--- app.py
+++ app.py
@@ -56,14 +56,12 @@ app.index_string = '''
         {%css%}
     </head>
     <body>
-        <div>My Custom header</div>
         {%app_entry%}
         <footer>
             {%config%}
             {%scripts%}
             {%renderer%}
         </footer>
-        <div>My Custom footer</div>
     </body>
 </html>
 '''
